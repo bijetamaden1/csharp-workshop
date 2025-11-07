b04ed3efcb737111260b5811e5db9f0a2931cabf
bijeta <bijetamaden@gmail.com>
2025-11-07 10:28:52 +0545

    task 1

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Task1
+{
+    class  workShop()
+    {
+        static void Main()
+        {
+            String userName = "Bijeta Maden";
+            int luckyNumber = 4;
+            Console.WriteLine($"Hello, {userName}! your lucky number is {luckyNumber}");
+        }
+    }
+}
